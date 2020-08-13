f44c29704492623a7aaf0b87dc041f8ce7e35042
abhij <abhij@Abhi-HP-Pav15>
2020-08-13 13:48:58 -0400

    First Eclipse Commit

---
+++ GITknowledge/src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+//rahulonlinetutor@gmail.com
+public class RESTAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira2");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");	
+	}
+}
